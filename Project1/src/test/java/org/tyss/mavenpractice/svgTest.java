617eaf851d0732093bbdc78afa0f2503bf36eef1
sonubai <sonubaipatil1234@gmail.com>
2022-08-03 12:54:48 +0530

    first

---
+++ Project1/src/test/java/org/tyss/mavenpractice/svgTest.java
@@ -0,0 +1,16 @@
+package org.tyss.mavenpractice;
+
+import org.testng.annotations.Test;
+
+public class svgTest {
+
+@Test
+public void testing() {
+	System.out.println("testing software");
+	System.out.println("hellohello");
+	System.out.println("test");
+}
+
+ 
+
+}
